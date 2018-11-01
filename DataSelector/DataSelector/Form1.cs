5af06fa172dd854cf503fb4576b88a55c2e2690d
The-Ian <1goldenspider1@gmail.com>
2018-11-01 17:22:38 -0400

    Progress

---
+++ DataSelector/DataSelector/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            
+
+            if (comboBox1.SelectedItem.Equals("Veterinary"))
+            {
+                VeterinaryDataSet Vets = new VeterinaryDataSet();
+                DataTable vetDT = new DataTable();
+                vetDT = Vets.Billing; 
+
+                listBox1.Items.Clear();
+               
+               foreach(DataTable tables in Vets.Tables)
+                {
+                    listBox1.Items.Add(Vets);
+                }
+
+                //listBox1.Items.Add(vetDT.Columns.ToString());
+            }
+
+            else if (comboBox1.SelectedItem.Equals("Northwind"))
+            {
+
+            }
+
+            else if (comboBox1.SelectedItem.Equals("The Library"))
+            {
+
+            }
+
+            else
+            {
+
+            }
+        }
+    }
+}
